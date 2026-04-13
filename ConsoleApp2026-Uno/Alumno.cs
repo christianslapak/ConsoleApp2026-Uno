42b657103214e27e537ade9e84bef3bae6f37193
christian.slapak <christian.slapak@ort.edu.ar>
2026-04-12 21:18:37 -0300

    email en alumno

--- ConsoleApp2026-Uno/Alumno.cs
+++ ConsoleApp2026-Uno/Alumno.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp2026_Uno
 {
     internal class Alumno:Persona
     {
+        public required string Email{ get; set; }
         public int Legajo { get; set; }
         public string? Materia { get; set; } 
 
